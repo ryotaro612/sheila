907a1c7d5d3ca3dc0f07abd5917391b68bb7820a
Ryotaro <ryotaro612.ooo@gmail.com>
2025-03-16 21:22:27 +0900

    a

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::{io, result};
 use std::os::unix::net::UnixListener;
 pub mod server;
@@ -13,7 +14,19 @@ pub(crate) fn run(socket: String, args: crate::parser::ServerArgs) -> result::Re
     let mut result: Result<(), String> = Ok(());
     for stream in listner.incoming() {
         match stream {
-            Ok(stream) => {}
+            Ok(stream) => {
+                    let mut message = String::new();
+                    let mut s = stream;
+                    let res = s.read_to_string(&mut message);
+                    match res {
+                        Ok(_) => {
+                            log::info!("{message}");
+                        }
+                        Err(err) => {
+                            log::error!("error reading from stream: {err}");
+                        }
+                    }
+            }
             Err(err) => {
                 result = Err(err.to_string());
                 break;
@@ -22,3 +35,5 @@ pub(crate) fn run(socket: String, args: crate::parser::ServerArgs) -> result::Re
     }
     result
 }
+
+
